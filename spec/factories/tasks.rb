f00582e5700f9265e9f6626af5d053ddf10f153a
saitonaoki <reisa.naoki@icloud.com>
2021-10-10 17:06:01 +0900

    インデント修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :task do
-    # 下記の内容は実際に作成するカラム名に合わせて変更してください
     task_name {'test_title'}
     content { 'test_content' }
     limit { "2021-10-10" }
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -3,12 +3,6 @@ RSpec.describe 'タスク管理機能', type: :system do
   before do
     FactoryBot.create(:task)
     FactoryBot.create(:second_task)
-    # メモのため
-    # visit new_task_path
-    # fill_in 'task[task_name]', with: 'task'
-    # fill_in 'task[content]', with: 'task_content'
-    # # fill_in select '完了',from: 'task[status]'
-    # click_button "Create my account"
   end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
@@ -41,7 +35,6 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
     context 'タイトルのあいまい検索とステータス検索をした場合' do
       it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
-        # ここに実装する
         visit tasks_path
         fill_in 'task[task_name]',with: 'test'
         select '完了',from: 'task[status]'
@@ -65,19 +58,13 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content '高'
       end
     end
-  
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
        visit tasks_path
        task_list = all('.task_list')
-      #  binding.irb
         expect(task_list[0]).to have_content 'test'
       end
     end
-
-       
-
-
 
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
@@ -90,5 +77,5 @@ RSpec.describe 'タスク管理機能', type: :system do
        expect(page).to have_content 'rails'
       end
     end
- end
+  end
 end
