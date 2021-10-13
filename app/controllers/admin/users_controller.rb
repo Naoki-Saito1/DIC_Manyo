ca9f62835b6237cd84688f37fce8c5041780612a
saitonaoki <reisa.naoki@icloud.com>
2021-10-14 00:53:45 +0900

    失敗したので一部作り直し

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,34 +1,29 @@
 class Admin::UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
-  skip_before_action :login_required, only: [:new, :create]
-  # before_action :admin_user
+
+  def index
+    @users = User.all.order("created_at asc")
+
+  end
   def new
     @user = User.new
   end
-
+  
   def create
-   @user = User.new(user_params)
-   if @user.save
-    session[:user_id] = user.id
-    redirect_to admin_user_path, notice: "登録しました"
-   end
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_user_path(@user.id)
+    else
+      render :new
+    end
   end
-
-  def index
-    @users = User.all.order("created_at DESC")
-  end
-
-private
-  # def admin_user
-  #   redirect_to(root_path) unless current_user.admin?
-  #   end
-  # end
-
-  def set_user
+  def shew
     @user = User.find(params[:id])
   end
-
-  def set_params
-    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation )
+  def edit
+    @user = User.find(params[:id])
   end
-end
+  private
+  def user_params
+      params.require(:user).permit(:user_name, :email, :admin, :password, :password_confirmation)
+  end
+end  
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_10_150712) do
+ActiveRecord::Schema.define(version: 2021_10_13_103000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2021_10_10_150712) do
     t.string "user_name"
     t.string "email"
     t.string "password_digest"
-    t.boolean "admin"
+    t.boolean "admin", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
