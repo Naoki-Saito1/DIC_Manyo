f89ceb8d2fc13df560068e065b5d85ee7c84e042
saitonaoki <reisa.naoki@icloud.com>
2021-10-14 12:00:44 +0900

    admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,26 +1,30 @@
 class Admin::UsersController < ApplicationController
-
+  skip_before_action :login_required, only: [:new, :create]
   def index
     @users = User.all.order("created_at asc")
-
   end
   def new
     @user = User.new
   end
-  
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to admin_user_path(@user.id)
-    else
-      render :new
+      session[:user_id] = @user.id
+      redirect_to admin_users_path 
+    
     end
   end
-  def shew
+
+  def show
     @user = User.find(params[:id])
   end
   def edit
     @user = User.find(params[:id])
+  end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path, notice: "削除しました"
   end
   private
   def user_params
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,10 @@
                password: password,
                )
 end
+
+
+User.create!(user_name:  "管理者",
+  email: "admin@example.jp",
+  password:  "11111111",
+  password_confirmation: "11111111",
+  admin: true)
